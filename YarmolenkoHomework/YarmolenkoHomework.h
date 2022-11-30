9d0339d6086a67daf0a1ade512e3365c0884184f
Daniil Yarmolenko <DaniilYarmolenko@gmail.com>
2022-11-30 16:38:31 +0300

    Initial Commit

---
+++ YarmolenkoHomework/YarmolenkoHomework.h
@@ -0,0 +1,18 @@
+//
+//  YarmolenkoHomework.h
+//  YarmolenkoHomework
+//
+//  Created by Даниил Ярмоленко on 30.11.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YarmolenkoHomework.
+FOUNDATION_EXPORT double YarmolenkoHomeworkVersionNumber;
+
+//! Project version string for YarmolenkoHomework.
+FOUNDATION_EXPORT const unsigned char YarmolenkoHomeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YarmolenkoHomework/PublicHeader.h>
+
+
